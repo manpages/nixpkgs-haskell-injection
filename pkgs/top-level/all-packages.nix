c32daf2ba6e941a780f288736539b3abe22437b5
Joel Moberg <joel.moberg@gmail.com>
2015-08-15 21:59:31 +0200

    rkrlv2: initial version at beta

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2852,6 +2852,8 @@ let
 
   rkflashtool = callPackage ../tools/misc/rkflashtool { };
 
+  rkrlv2 = callPackage ../applications/audio/rkrlv2 {};
+
   rmlint = callPackage ../tools/misc/rmlint {
     inherit (pythonPackages) sphinx;
   };
