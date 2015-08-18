11ae39b226f36dc4bc5f53a6ba1b2e0c5d84f5c4
Jonn Mostovoy <amarr.industrial@gmail.com>
2015-08-18 16:36:34 +0200

    Bump haskonf

--- pkgs/development/haskell-modules/injected/haskonf.nix
+++ pkgs/development/haskell-modules/injected/haskonf.nix
@@ -4,8 +4,8 @@
 mkDerivation {
   pname = "haskonf";
   version = "0.0.1";
-  src = fetchurl { url    = "https://github.com/manpages/haskonf/archive/e6662e158578006943ebd95381d6d7b89c657ed8.tar.gz";
-                   sha256 = "5f3660b65d44584d3245d8387fbbb9b1f8d3a5ffdef2d0d6e037dd69e51ea414"; };
+  src = fetchurl { url    = "https://github.com/manpages/haskonf/archive/86ba73867a3555725371732914e88a530e29ff4a.tar.gz";
+                   sha256 = "433a8c15a579721ac24f2f32199015115fb8f48a08a8bbbcb377f6c05277a604"; };
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
